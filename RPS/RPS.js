86aaf7b9276e620fc32bc3db20e2c12f388bd9c3
ThuVi <buihhh321@gmail.com>
2024-07-25 01:47:17 -0400

    create and test linked project

---
+++ RPS/RPS.js
@@ -0,0 +1,13 @@
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+}
+for(var i = 0; i < 6; i++){
+    var Cpu = getRandomInt(3);
+    var human = parseInt(prompt("enter your choice(Rock, Papper, Scissor): "));
+    
+}
+
+
+
+
