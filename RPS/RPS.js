606332601ac99ba99c2c57e0d2e37f7054258063
ThuVi <buihhh321@gmail.com>
2024-07-27 01:51:21 -0400

    finish

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -1,12 +1,84 @@
 
+var userP = 0;
+var cpuP = 0;
+
 function getRandomInt(max){
-    return Math.floor(Math.random() * max)
+    return Math.floor(Math.random() * max + 1)
 }
-for(var i = 0; i < 6; i++){
-    var Cpu = getRandomInt(3);
-    var human = parseInt(prompt("enter your choice(Rock, Papper, Scissor): "));
-    
+function Rps(human, Cpu){
+    if(human == Cpu){
+        console.log("it a tied");
+        userP++;
+        cpuP++;
+    }else if((human == 1 && Cpu == 2) || (human == 2 && Cpu == 3) || (human == 3 && Cpu == 1)){
+        console.log('You lose, Cpu Win');
+        cpuP++;
+    }else if((human == 1 && Cpu == 3) || (human == 2 && Cpu == 1) || (human == 3 && Cpu == 2)){
+        console.log('You win, Cpu lose');
+        userP++;
+    }
 }
+function welcome(){
+    console.clear();
+    console.log('Welcome to Rock Paper Scissor game');
+    console.log('Press 1 to choose \'Rock\'');
+    console.log('Press 2 to choose \'Paper\'');
+    console.log('Press 3 to choose \'Scissor\'');
+    console.log('-------------------------------');
+    console.log('Or press 0 to Exit');
+}
+var values = [1,2,3];
+
+function getInput(n,text){
+    while(true){
+        var user = parseInt(prompt(text));
+        if(!n.includes(user)){
+            console.log("Not a valid input, try again!")
+        }else{
+            break;
+        }
+    }
+    return user
+}
+
+function round(){
+    while(true){
+        console.clear();
+        var round = parseInt(prompt("How many round you want to play? Enter a number: "));
+        if(round <= 0 || isNaN(round)){
+            console.log("Not a valid input, try again!")
+        }else{
+            break;
+        }
+    }
+    return round
+}
+
+
+
+var userInputValue = [1,2];
+var userInput = round();
+for(var i = 1; i <= userInput; i++){
+    welcome();
+    Rps(getInput(values,"enter your choice: "), getRandomInt(3));
+    if( i == userInput){
+        var ques = getInput(userInputValue,"Play again?(1 for yes, 2 for no): ");
+        if(ques == 1){
+    console.log(`Score: You ${userP} - ${cpuP} Cpu`);
+            i = 0;
+            userInput = round();
+            userP = 0;
+            cpuP = 0;
+        }else if(ques == 2){
+            break;
+        }
+    }
+}
+console.log(`Score: You ${userP} - ${cpuP} Cpu`);
+
+
+
+
 
 
 
