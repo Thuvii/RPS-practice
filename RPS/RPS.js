a89178f36e457f6182b6604e76651e14cbccaf24
ThuVi <buihhh321@gmail.com>
2024-07-27 22:58:35 -0400

    change function and variables name (clean code)

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -1,6 +1,6 @@
 
-var userP = 0;
-var cpuP = 0;
+var userPoint = 0;
+var cpuPoint = 0;
 
 function getRandomInt(max){
     return Math.floor(Math.random() * max + 1)
@@ -8,17 +8,17 @@ function getRandomInt(max){
 function Rps(human, Cpu){
     if(human == Cpu){
         console.log("it a tied");
-        userP++;
-        cpuP++;
+        userPoint++;
+        cpuPoint++;
     }else if((human == 1 && Cpu == 2) || (human == 2 && Cpu == 3) || (human == 3 && Cpu == 1)){
         console.log('You lose, Cpu Win');
-        cpuP++;
+        cpuPoint++;
     }else if((human == 1 && Cpu == 3) || (human == 2 && Cpu == 1) || (human == 3 && Cpu == 2)){
         console.log('You win, Cpu lose');
-        userP++;
+        userPoint++;
     }
 }
-function welcome(){
+function welcomeMessage(){
     console.clear();
     console.log('Welcome to Rock Paper Scissor game');
     console.log('Press 1 to choose \'Rock\'');
@@ -27,9 +27,13 @@ function welcome(){
     console.log('-------------------------------');
     console.log('Or press 0 to Exit');
 }
-var values = [1,2,3];
 
-function getInput(n,text){
+function resultMessage(a,b){
+    console.log(`Score: You ${a} - ${b} Cpu`);
+}
+
+var values = [1,2,3];
+function getUserInput(n,text){
     while(true){
         var user = parseInt(prompt(text));
         if(!n.includes(user)){
@@ -41,7 +45,7 @@ function getInput(n,text){
     return user
 }
 
-function round(){
+function roundCount(){
     while(true){
         var round = parseInt(prompt("How many round you want to play? Enter a number: "));
         if(round <= 0 || isNaN(round)){
@@ -53,32 +57,35 @@ function round(){
     return round
 }
 
+
+
 function playGame(){
     var userInputValue = [1,2];
-    var userInput = round();
-    welcome();
+    var userInput = roundCount();
+    welcomeMessage();
     for(var i = 1; i <= userInput; i++){
-        Rps(getInput(values,"enter your choice: "), getRandomInt(3));
+        Rps(getUserInput(values,"enter your choice: "), getRandomInt(3));
         if( i == userInput){
-            var ques = getInput(userInputValue,"Play again?(1 for yes, 2 for no): ");
+            var ques = getUserInput(userInputValue,"Play again?(1 for yes, 2 for no): ");
             if(ques == 1){
+                resultMessage(userPoint,cpuPoint);
                 i = 0;
-                userInput = round();
-                userP = 0;
-                cpuP = 0;
+                userInput = roundCount();
+                userPoint = 0;
+                cpuPoint = 0;
             }else if(ques == 2){
                 break;
             }
         }
     }
-    if(userP > cpuP){
-        console.log(`Score: You ${userP} - ${cpuP} Cpu`);
+    if(userPoint > cpuPoint){
+        resultMessage(userPoint,cpuPoint);
         console.log('You WIN');
-    }else if(cpuP > userP){
-        console.log(`Score: You ${userP} - ${cpuP} Cpu`);
+    }else if(cpuPoint > userPoint){
+        resultMessage(userPoint,cpuPoint);
         console.log('You LOSE');
     }else{
-        console.log(`Score: You ${userP} - ${cpuP} Cpu`);
+        resultMessage(userPoint,cpuPoint);
         console.log('It a TIED');
     }
 }
