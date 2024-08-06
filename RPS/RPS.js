68652bdea8291cc7c6bcc9c5dd8af7480f26b499
Thuvii <buihhh321@gmail.com>
2024-08-06 02:34:16 -0400

    finish UI rps game

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -17,19 +17,20 @@ function Rps(human, Cpu){
     document.querySelector("#cpuP").innerText = `player point: ${cpuPoint}`;
 }
 function resultMessage(a,b){
+    var result = document.getElementById('result');
     document.querySelector('#annouce').innerText = `Score: You ${a} - ${b} Cpu`;
-    if(userPoint > cpuPoint){
-        const newDiv = document.createElement('div');
-        
-    }else if(cpuPoint > userPoint){
-        console.log('You LOSE');
-    }else{  
-        console.log('It a TIED');
-    }
+    
 }
 function playGame(playerInput){
 
     Rps(playerInput, getRandomInt(3));
+    if(userPoint > cpuPoint){
+        result.innerText = 'You win';
+    }else if(cpuPoint > userPoint){
+        result.innerText = 'You lose';
+    }else{  
+        result.innerText = 'It\'s a tied';
+    }
     if(userPoint == 5 || cpuPoint == 5){
         resultMessage(userPoint,cpuPoint);
         buttonToggle(false);
@@ -39,8 +40,8 @@ function playGame(playerInput){
 function resetGame() {
     userPoint = 0;
     cpuPoint = 0;
-    document.getElementById('playerP').innerText = userPoint;
-    document.getElementById('cpuP').innerText = cpuPoint;
+    document.getElementById('playerP').innerText = `player point: ${userPoint}`;
+    document.getElementById('cpuP').innerText = `player point: ${cpuPoint}`;
     document.getElementById('annouce').innerText = "";
     buttonToggle(true);
 }
